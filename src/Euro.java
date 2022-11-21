b2382d8d4c89a35f35126c99509b69512678d094
Dekrate <mikdiakowski@interia.pl>
2022-11-21 17:42:35 +0100

    Initial commit

---
+++ src/Euro.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+
+public class Euro implements Currency {
+    private final float value;
+
+    public Euro(float value) {
+        this.value = value;
+    }
+
+    @Override
+    public Currency addedCurrency(float value, String currency) {
+        FakeCantor fakeCantor = new FakeCantor();
+        float v = fakeCantor.euroToRate(currency);
+        return new Euro(this.value + value*v);
+    }
+
+    @Override
+    public Currency subtractedCurrency(float value, String currency) {
+        FakeCantor fakeCantor = new FakeCantor();
+        float v = fakeCantor.euroToRate(currency);
+        float result = this.value - v*value;
+        if (result > 0) {
+            return new Euro(result);
+        } else {
+            throw new ArithmeticException("How dare you get in-debted?"){{System.out.println(getMessage());}};
+        }
+    }
+
+    @Override
+    public String abbreviation() {
+        return "EUR";
+    }
+
+    @Override
+    public String symbol() {
+        return "€";
+    }
+
+    @Override
+    public String balance() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public float toDollarExchangeRate() {
+        FakeCantor fakeCantor = new FakeCantor();
+        return fakeCantor.euroToRate("USD");
+    }
+}
